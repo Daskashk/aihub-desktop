e0323bb2f9ef7129a22dea802a2596de7d275d52
Daskashk <130722296+Daskashk@users.noreply.github.com>
2026-04-02 08:00:58 -0400

    Refactor preload.js to streamline API functions

--- preload.js
+++ preload.js
@@ -1,37 +1,18 @@
-// preload.js - Puente seguro entre frontend y backend
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Exponer funciones seguras al frontend
 contextBridge.exposeInMainWorld('electronAPI', {
-    // Obtener configuración actual
+    // Configuration
     getConfig: () => ipcRenderer.invoke('get-config'),
+                                saveConfig: (config) => ipcRenderer.invoke('save-config', config),
 
-                                // Actualizar datos remotos
+                                // Services
+                                getServices: () => ipcRenderer.invoke('get-services'),
+                                getRules: () => ipcRenderer.invoke('get-rules'),
                                 updateRemoteData: () => ipcRenderer.invoke('update-remote-data'),
 
-                                // Obtener lista de servicios
-                                getServices: () => ipcRenderer.invoke('get-services'),
+                                // Service Management
+                                toggleService: (serviceId) => ipcRenderer.invoke('toggle-service', serviceId),
 
-                                // Obtener reglas de dominios
-                                getRules: () => ipcRenderer.invoke('get-rules'),
-
-                                // Actualizar configuración
-                                updateConfig: (newConfig) => ipcRenderer.invoke('update-config', newConfig),
-
-                                // Abrir servicio
-                                openService: (serviceId, serviceUrl, serviceName) =>
-                                ipcRenderer.invoke('open-service', serviceId, serviceUrl, serviceName),
-
-                                // Cerrar servicio
-                                closeService: (serviceId) => ipcRenderer.invoke('close-service', serviceId),
-
-                                // Alternar bloqueo
-                                toggleBlocking: (enabled) => ipcRenderer.invoke('toggle-blocking', enabled),
-
-                                // Escuchar eventos desde el backend
-                                onMaxServicesReached: (callback) =>
-                                ipcRenderer.on('max-services-reached', (event, maxServices) => callback(maxServices)),
-
-                                onServiceClosed: (callback) =>
-                                ipcRenderer.on('service-closed', (event, serviceId) => callback(serviceId))
+                                // Tab Management
+                                setActiveService: (serviceId) => ipcRenderer.send('set-active-service', serviceId)
 });
